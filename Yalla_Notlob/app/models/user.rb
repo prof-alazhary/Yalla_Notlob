9cfb1b215e5bb4fdaaa5dec5acd822214b39ec76
HagerElsayed <hagerelsayed39@gmail.com>
2017-03-29 23:59:01 +0200

    Login And Registration Using Device Gem

--- Yalla_Notlob/app/models/user.rb
+++ Yalla_Notlob/app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :friends ,
-  class_name: "User" ,join_table :friendships ,foreign_key:"user_id",association_foreign_key:"friend_user_id"
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_and_belongs_to_many :friends
   has_many :groups
   has_many :orders
   has_many :order_details
-  has_and_belongs_to_many :groups
+  has_and_belongs_to_many :groups,
+  #class_name: "User",join_table :friendships ,foreign_key:"user_id",association_foreign_key:"friend_user_id"
+  class_name: "User",
+  join_table:  :friendships,
+  foreign_key: :user_id,
+  association_foreign_key: :friend_user_id
+
 end
