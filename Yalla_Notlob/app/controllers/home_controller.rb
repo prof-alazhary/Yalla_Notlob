60131181a6be51883b50bf44bf069ed8b307dc9a
HagerElsayed <hagerelsayed39@gmail.com>
2017-03-31 15:26:53 +0200

    Styling Home,Groups&Friends pages

---
+++ Yalla_Notlob/app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    puts 'Home'
+  end
+end
--- Yalla_Notlob/config/routes.rb
+++ Yalla_Notlob/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :friends
   resources :users do
       resources :orders do
             resources :order_details
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
       resources :groups
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root to: "home#index"
 end
