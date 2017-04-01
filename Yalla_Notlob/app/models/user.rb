50b2a754bb7895885f387e05f07d59ab2aeb532e
Mohammed Al-Azhary <prof.alazhary@gmail.com>
2017-04-01 20:14:30 +0200

    save master

--- Yalla_Notlob/app/models/user.rb
+++ Yalla_Notlob/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :friends ,
-  class_name: "User" ,join_table :friendships ,foreign_key:"user_id",association_foreign_key:"friend_user_id"
-  has_many :groups
+  class_name: "User" , join_table: :friendships , foreign_key: :user_id , association_foreign_key: :friend_user_id
   has_many :orders
   has_many :order_details
   has_and_belongs_to_many :groups
--- Yalla_Notlob/db/migrate/20170328131619_create_users_groups.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersGroups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users_groups do |t|
-        t.references :user, foreign_key: true
-        t.references :group, foreign_key: true 
-    end
-  end
-end
