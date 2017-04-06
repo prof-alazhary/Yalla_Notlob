3bd2c60a9f22cf09fcf0a5b898fedb3576ca3dd1
IslamWahid <eng.islamwahid@gmail.com>
2017-04-06 21:13:09 +0200

    upload order menu, prepare for invitation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,7 @@ class OrdersController < ApplicationController
     # @order = Order.new(order_params)
     @user=User.find params[:user_id]
     @order = @user.orders.new(order_params)
+    byebug
 
     respond_to do |format|
       if @order.save
@@ -73,6 +74,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:meal, :rest_name, :menu_img, :user_id)
+      params.require(:order).permit(:meal, :rest_name, :menu_img, :menu_img, :menu_img_cache, :user_id)
     end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details
+
+    mount_uploader :menu_img, MenuImgUploader
 end
