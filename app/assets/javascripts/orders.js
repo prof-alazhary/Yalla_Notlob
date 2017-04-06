ce83f184f1ca3ef69261ade0cf66d84089437586
Mohammed Al-Azhary <prof.alazhary@gmail.com>
2017-04-07 00:56:13 +0200

    "add order.js"

---
+++ app/assets/javascripts/orders.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  $('#invite_friend').keyup(function (e) {
+      if (e.keyCode==32) {
+          console.log($(this).val())
+          $.ajax({
+            url: "/invite_friend",
+            method: "post",
+            data: {email: $(this).val() },
+            success: function(result){
+              if (result.name!=undefined) {
+                console.log(result);
+                $("#friends").prepend(`
+                    <div class="col-sm-3">
+                    <h4>`+result.name+`</h4>
+                    <img src="`+result.img+`" width="80px" height="80px" class="img-thumbnail"/>
+                  <br/>
+                      <br>
+                      <a val=`+result.id+` href="" class='btn btn-danger'>
+                      <i class="icon-remove icon-white"></i>
+                      remove
+                      </a>
+                      <input type="hidden" name="order[friends][]" value=`+result.id+` >
+                      </div>
+                </div>`)
+              }else {
+                alert(result+" not valid email!");
+              }
+              $(this).val('')
+            },
+            error: function(error) {
+              alert(error);
+           }
+        });
+      }
+  })
+
+})
