93eb7d058c108caf750064c80c169733084ee96d
HagerElsayed <hagerelsayed39@gmail.com>
2017-04-10 21:57:32 +0200

    add mail beside avatar if they signin with SocialMedia

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
      user.email = auth.info.email
      user.password = Devise.friendly_token[0,20]
+     user.name=auth.info.name
+     user.save!
+     user
       end
     end
     # searchkick word_start: [:email, :name]
@@ -22,7 +25,7 @@
     has_many :order_details
     has_many :groups
     # has_and_belongs_to_many :groups
-    
+
     #class_name: "User",join_table :friendships ,foreign_key:"user_id",association_foreign_key:"friend_user_id"
     #has_and_belongs_to_many :friends,class_name: "User",join_table:  :friendships,foreign_key: :user_id,association_foreign_key: :friend_user_id
     has_many :friendships
